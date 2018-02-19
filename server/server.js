4385e1c7c9fd2d3cc9fbb3b7cac367c5721a3015
naseem11 <miannaseem11@gmail.com>
2018-02-19 22:06:42 +0000

    Added route to GET single todo

--- server/server.js
+++ server/server.js
@@ -1,35 +1,37 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-
-var {mongoose} = require('./db/mongoose');
-var {User} = require('./models/user');
-var {Todo} = require('./models/todo');
+var express = require('express');
+var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 
-var app =express();
+var { mongoose } = require('./db/mongoose');
+var { User } = require('./models/user');
+var { Todo } = require('./models/todo');
+
+
+var app = express();
 
 app.use(bodyParser.json());
 
 // POST  todo.......................................//////
 
-app.post('/todos',(req,res)=>{
+app.post('/todos', (req, res) => {
 
-   var todo=new Todo({
+    var todo = new Todo({
 
-    text: req.body.text
+        text: req.body.text
 
-   });
+    });
 
-   todo.save().then((doc)=>{
+    todo.save().then((doc) => {
 
-    res.send(doc);
+        res.send(doc);
 
-   },(err)=>{
+    }, (err) => {
 
-    res.status(400).send(err);
-   
+        res.status(400).send(err);
 
-   });
+
+    });
 
 });
 
@@ -37,27 +39,58 @@ app.post('/todos',(req,res)=>{
 
 // get all Todos.......................................////////////////////
 
-app.get('/todos',(req,res)=>{
+app.get('/todos', (req, res) => {
 
-    Todo.find().then((todos)=>{
+    Todo.find().then((todos) => {
 
-        res.send({todos});
+        res.send({ todos });
 
-    },(err=>{
+    }, (err => {
 
-    res.status(400).send(err);
+        res.status(400).send(err);
 
     }));
 
 });
 
 
-app.listen(3000,()=>{
-console.log('Server is up on port 3000');
+// Getting a single todo.......
+
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    if (!(ObjectID.isValid(id))) {
+
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+
+        if (!todo) {
+            return res.status(404).send();
+
+        }
+        res.send({todo});
+
+    }, (err) => {
+
+        res.status(400).send();
+    });
+
+
+
+
 
 });
 
-module.exports={app};
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+
+});
+
+module.exports = { app };
 
 
 
